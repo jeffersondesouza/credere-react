35b369080eb3ced500b027db063c31408ad0693d
jeffersondesouza <jeffersondesouzacordel@gmail.com>
2018-12-27 06:15:00 -0300

    form grouo ok

--- src/components/ui/FormGroup/index.js
+++ src/components/ui/FormGroup/index.js
@@ -1,15 +1,9 @@
 import React from 'react';
-import EmailInput from '../EmailInput';
 
 const FormGroup = (props) => (
-  <div>
-    <ul id="emails">
-      <li>
-        <EmailInput name="email" />
-      </li>
-    </ul>
-    <p id="minEmailValidation" hidden className="feedback-error">Por favor, informe pelo menos um email</p>
-    <p id="maxEmailValidation" hidden className="feedback-error">Você pode cadastrar no máxio 3 (três) emails</p>
+  <div className="form-group">
+    <label>{props.label}</label>
+    {props.children}
   </div>
 );
 
